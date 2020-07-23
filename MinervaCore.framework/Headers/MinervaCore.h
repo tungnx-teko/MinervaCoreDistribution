4aaf77958129ddfdf4e7365cb7c53df61994c254
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2020-07-23 16:28:02 +0700

    Init

---
+++ MinervaCore.framework/Headers/MinervaCore.h
@@ -0,0 +1,18 @@
+//
+//  MinervaCore.h
+//  MinervaCore
+//
+//  Created by Tung Nguyen on 7/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MinervaCore.
+FOUNDATION_EXPORT double MinervaCoreVersionNumber;
+
+//! Project version string for MinervaCore.
+FOUNDATION_EXPORT const unsigned char MinervaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MinervaCore/PublicHeader.h>
+
+
